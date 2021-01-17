65c476bbbda0847d8faa5aeb74997ac2fcc0b998
CesarFCL <57689841+CesarFCL@users.noreply.github.com>
2021-01-17 15:21:58 -0500

    WordCloud con Scrapping V.2
    Se consiguió unir la lista de etiquetas con la lista de la frecuencia de las etiquetas.

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import requests
 import pandas as pd
 import numpy as np
 
-#URL (Esta parte hay q cambiarla, se debe ingresar la URL y que busque al usuario)
+#URL del perfil con las etiquetas de SO (Esta parte hay q cambiarla, se debe ingresar la URL y que busque al usuario)
+
 url = "https://es.stackoverflow.com/users/22851/pablo-lozano?tab=tags"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-
 
 #Scrapping de etiquetas
 et = soup.find_all('a', class_='post-tag')
 frec = soup.find_all('span', class_='item-multiplier-count')
 
-#lista etiquetas
+#lista etiqueta
 etiquetas = list()
 for i in et:
     etiquetas.append(i.text)
@@ -23,21 +23,17 @@ for i in et:
 #lista frecuencia de etiquetas
 frecuencia = list()
 for i in frec:
-    frecuencia.append(i.text)
+    #Aquí añadimos int() porque el método generate_from_frequencies() necesita enteros no string
+    frecuencia.append(int(i.text))
 
-#Se comprueba que la cantidad de etiquetas y valores de frecuencia sea el mismo (Esta parte se tendra que quitar)
-print(etiquetas)
-print(len(etiquetas))
-print(frecuencia)
-print(len(frecuencia))
+#Aquí utilizamos zip para unir las etiquetas con sus repsectivas frecuencias
+etfrecuencias = dict(zip(etiquetas, frecuencia))
 
-#La lista etiquetas pasa a texto (Hay que cambiar esta parte ya que no se toma en cuenta la frecuencia para hacer el WordCloud y deben tomarse en cuenta)
-text= " ".join(etiquetas)
-
-#Se genera el WordCloud con las etiquetas
-cloud = WordCloud(background_color="white").generate(text)
+#Utilizamos el método .generate_from_frequencies pasandole las frecuencias
+cloud = WordCloud(background_color="white").generate_from_frequencies(etfrecuencias)
 
 #Se presenta el WordCloud
-plt.imshow(cloud)
-plt.axis('off')
+plt.figure()
+plt.imshow(cloud, interpolation="bilinear")
+plt.axis("off")
 plt.show()
