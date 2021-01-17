42fd4ab1ea814ebb86c6e543d6017da9335bdbd6
CesarFCL <57689841+CesarFCL@users.noreply.github.com>
2021-01-17 15:14:16 -0500

    WordCloud con Scrapping V.1
    Se realiza el scrapping de etiquetas, pero al generar el WordCloud solo se toma en cuenta las etiquetas y no su frecuencia.

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-url = "https://es.stackoverflow.com/users/153371/franacuna?tab=tags"
+import numpy as np
+
+#URL (Esta parte hay q cambiarla, se debe ingresar la URL y que busque al usuario)
+url = "https://es.stackoverflow.com/users/22851/pablo-lozano?tab=tags"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-#etiquetas
+
+#Scrapping de etiquetas
 et = soup.find_all('a', class_='post-tag')
+frec = soup.find_all('span', class_='item-multiplier-count')
 
+#lista etiquetas
 etiquetas = list()
+for i in et:
+    etiquetas.append(i.text)
 
-for a in et:
-    etiquetas.append(a.text)
+#lista frecuencia de etiquetas
+frecuencia = list()
+for i in frec:
+    frecuencia.append(i.text)
 
+#Se comprueba que la cantidad de etiquetas y valores de frecuencia sea el mismo (Esta parte se tendra que quitar)
 print(etiquetas)
+print(len(etiquetas))
+print(frecuencia)
+print(len(frecuencia))
+
+#La lista etiquetas pasa a texto (Hay que cambiar esta parte ya que no se toma en cuenta la frecuencia para hacer el WordCloud y deben tomarse en cuenta)
+text= " ".join(etiquetas)
+
+#Se genera el WordCloud con las etiquetas
+cloud = WordCloud(background_color="white").generate(text)
+
+#Se presenta el WordCloud
+plt.imshow(cloud)
+plt.axis('off')
+plt.show()
