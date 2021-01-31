73203599012d21b576d68563107fabd2535be005
CesarFCL <57689841+CesarFCL@users.noreply.github.com>
2021-01-30 19:56:50 -0500

    WordCloud con Scrapping V.6
    Se agrego el mensaje de error cuando la ID de usuario introducida es invalida y se arreglo el error de que a veces no no tomaban en cuanta correctamente las etiquetas de frecuencia y el WC no se presentaba como correspondía.

--- main.py
+++ main.py
@@ -5,54 +5,62 @@ import requests
 import pandas as pd
 import numpy as np
 from PIL import Image
+while True:
+    try:
+        # Ingreso de ID de usuario de StackOverflow por teclado
+        print("Introduzca la ID del usuario: ")
+        ID = input()
 
-# Ingreso de ID de usuario de StackOverflow por teclado
-print("Introduzca la ID del usuario: ")
-ID = input()
+        # Se obtiene la pagina de etiquetas a partir de la URL del usuario
+        url = f'https://es.stackoverflow.com/users/{ID}?tab=tags'
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
 
-# Se obtiene la pagina de etiquetas a partir de la URL del usuario
-url = f'https://es.stackoverflow.com/users/{ID}?tab=tags'
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+        # Scrapping de etiquetas
+        et = soup.find_all('a', class_='post-tag')
+        frec = soup.find_all('td')
 
-# Scrapping de etiquetas
-et = soup.find_all('a', class_='post-tag')
-frec = soup.find_all('span', class_='item-multiplier-count')
+        # lista etiqueta
+        etiquetas = list()
+        for i in et:
+            etiquetas.append(i.text)
+        # Hay veces en las que un perfil muestra la etiqueta principal y al tomar los datos esa etiqueta queda repetida.
+        # Con este if nos aseguramos que en caso de que se este tomando una etiqueta principal, esta se elimina para que no cause irregularidades.
+        if etiquetas[0] == etiquetas[1]:
+            etiquetas.pop(0)
 
-# lista etiqueta
-etiquetas = list()
-for i in et:
-    etiquetas.append(i.text)
-# Hay veces en las que un perfil muestra la etiqueta principal y al tomar los datos esa etiqueta queda repetida.
-# Con este if nos aseguramos que en caso de que se este tomando una etiqueta principal, esta se elimina para que no cause irregularidades.
-if etiquetas[0] == etiquetas[1]:
-    etiquetas.pop(0)
+        # lista frecuencia de etiquetas
+        frecuencia = list()
+        for i in frec:
+            if i.text[-1:] !='\n':
+                frecuencia.append(int(i.text[-2:]))
+            else:
+                frecuencia.append(1)
+        print(frecuencia)
 
-# lista frecuencia de etiquetas
-frecuencia = list()
-for i in frec:
-    # Aquí añadimos int() porque el método generate_from_frequencies() necesita enteros no string
-    frecuencia.append(int(i.text))
+        # Aquí utilizamos zip para unir las etiquetas con sus repsectivas frecuencias
+        etfrecuencias = dict(zip(etiquetas, frecuencia))
+        print(etfrecuencias)
 
-# Aquí utilizamos zip para unir las etiquetas con sus repsectivas frecuencias
-etfrecuencias = dict(zip(etiquetas, frecuencia))
+        # Se establece que imagen se usara para el WordCloud, descomentar linea de código siguiente si esque se usa imagen
+        # custom_mask = np.array(Image.open("mancha.jpg"))
 
-# Se establece que imagen se usara para el WordCloud, descomentar linea de código siguiente si esque se usa imagen
-custom_mask = np.array(Image.open("mancha.jpg"))
+        # Utilizamos el método .generate_from_frequencies pasandole las frecuencias
 
-# Utilizamos el método .generate_from_frequencies pasandole las frecuencias
+        # Con imagen "mancha.jpg"
+        #cloud = WordCloud(background_color="black", scale=2, relative_scaling=0.1, mask=custom_mask, contour_width=1, contour_color='steelblue').generate_from_frequencies(etfrecuencias)
 
-# Con imagen "mancha.jpg"
-cloud = WordCloud(background_color="black", scale=2, relative_scaling=0.1, mask=custom_mask, contour_width=1, contour_color='steelblue').generate_from_frequencies(etfrecuencias)
+        # Sin imagen "mancha.jpg"
+        cloud = WordCloud(background_color="black", scale=2, relative_scaling=0.1, contour_width=1,contour_color='steelblue').generate_from_frequencies(etfrecuencias)
 
-# Sin imagen "mancha.jpg", si se utiliza esto hay q comentar el cloud anterior y el custom_mask
-#cloud = WordCloud(background_color="black", scale=2, relative_scaling=0.1, contour_width=1,contour_color='steelblue').generate_from_frequencies(etfrecuencias)
+        # Se guarda la imagen
+        cloud.to_file('wcloud.png')
 
-# Se guarda la imagen
-cloud.to_file('wcloud.png')
-
-# Se presenta el WordCloud
-plt.figure()
-plt.imshow(cloud, interpolation="bilinear")
-plt.axis("off")
-plt.show()
+        # Se presenta el WordCloud
+        plt.figure()
+        plt.imshow(cloud, interpolation="bilinear")
+        plt.axis("off")
+        plt.show()
+        break
+    except:
+        print("ID de usuario invalida")
